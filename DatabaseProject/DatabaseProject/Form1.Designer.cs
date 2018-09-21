86f427c0a77ac1d7247f17f0c59baab9d175601e
mattj241 <londonm241@gmail.com>
2018-09-20 22:08:38 -0400

    UI and initial query complete

--- DatabaseProject/DatabaseProject/Form1.Designer.cs
+++ DatabaseProject/DatabaseProject/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.QueryBox = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(12, 123);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(776, 315);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input SQL Query:";
+            // 
+            // QueryBox
+            // 
+            this.QueryBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QueryBox.Location = new System.Drawing.Point(17, 66);
+            this.QueryBox.Name = "QueryBox";
+            this.QueryBox.Size = new System.Drawing.Size(265, 22);
+            this.QueryBox.TabIndex = 2;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(314, 66);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 22);
+            this.SubmitButton.TabIndex = 3;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.QueryBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DataGridView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox QueryBox;
+        private System.Windows.Forms.Button SubmitButton;
     }
 }
 
--- DatabaseProject/DatabaseProject/Form1.cs
+++ DatabaseProject/DatabaseProject/Form1.cs
@@ -1,20 +1,54 @@
 ﻿using System;
+using System.Xml;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//SELECT * FROM Cars
+
 namespace DatabaseProject
 {
     public partial class Form1 : Form
     {
+        SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\londo\\Documents\\GitHub\\CIS425\\CIS425\\DatabaseProject\\DatabaseProject\\Database1.mdf;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader reader;
+        DataTable dataTable = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            if (QueryBox.TextLength == 0)
+            {
+                MessageBox.Show("Enter a Query!");
+            }
+
+            else
+            {
+                cmd.CommandText = QueryBox.Text;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+
+                connection.Open();
+                reader = cmd.ExecuteReader();
+                dataTable.Load(reader);
+
+                DataGridView.DataSource = dataTable;
+                DataGridView.Update();
+
+                connection.Close();
+            }
+
         }
     }
 }
