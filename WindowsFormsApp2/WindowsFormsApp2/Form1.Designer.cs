c6791b29ac6fe8120e5b03ad5f80450c9c4a0255
mattj241 <londonm241@gmail.com>
2018-11-10 00:32:39 -0500

    Select * from {table} is working correctly except...
    Somehow losing the DriversLicenseNumber from my tables on the join....

--- WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -28,68 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-               this.submitBtn = new System.Windows.Forms.Button();
-               this.sql_input = new System.Windows.Forms.TextBox();
-               this.sql_Output = new System.Windows.Forms.DataGridView();
-               this.dataGridView1 = new System.Windows.Forms.DataGridView();
-               ((System.ComponentModel.ISupportInitialize)(this.sql_Output)).BeginInit();
-               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-               this.SuspendLayout();
-               // 
-               // submitBtn
-               // 
-               this.submitBtn.Location = new System.Drawing.Point(558, 9);
-               this.submitBtn.Name = "submitBtn";
-               this.submitBtn.Size = new System.Drawing.Size(128, 52);
-               this.submitBtn.TabIndex = 1;
-               this.submitBtn.Text = "Submit";
-               this.submitBtn.UseVisualStyleBackColor = true;
-               this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
-               // 
-               // sql_input
-               // 
-               this.sql_input.AcceptsReturn = true;
-               this.sql_input.Location = new System.Drawing.Point(13, 9);
-               this.sql_input.Name = "sql_input";
-               this.sql_input.Size = new System.Drawing.Size(539, 22);
-               this.sql_input.TabIndex = 2;
-               this.sql_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.sql_input_KeyDown);
-               // 
-               // sql_Output
-               // 
-               this.sql_Output.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-               this.sql_Output.Location = new System.Drawing.Point(12, 69);
-               this.sql_Output.Name = "sql_Output";
-               this.sql_Output.RowTemplate.Height = 24;
-               this.sql_Output.Size = new System.Drawing.Size(665, 292);
-               this.sql_Output.TabIndex = 3;
-               // 
-               // dataGridView1
-               // 
-               this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-               this.dataGridView1.Location = new System.Drawing.Point(701, 69);
-               this.dataGridView1.Name = "dataGridView1";
-               this.dataGridView1.RowTemplate.Height = 24;
-               this.dataGridView1.Size = new System.Drawing.Size(665, 292);
-               this.dataGridView1.TabIndex = 4;
-               this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-               // 
-               // carRental
-               // 
-               this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-               this.ClientSize = new System.Drawing.Size(1422, 383);
-               this.Controls.Add(this.dataGridView1);
-               this.Controls.Add(this.sql_Output);
-               this.Controls.Add(this.sql_input);
-               this.Controls.Add(this.submitBtn);
-               this.Name = "carRental";
-               this.Text = "Car Rental";
-               this.Load += new System.EventHandler(this.carRental_Load);
-               ((System.ComponentModel.ISupportInitialize)(this.sql_Output)).EndInit();
-               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-               this.ResumeLayout(false);
-               this.PerformLayout();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.sql_input = new System.Windows.Forms.TextBox();
+            this.sql_Output = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.MergedView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.sql_Output)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MergedView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(418, 7);
+            this.submitBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(96, 42);
+            this.submitBtn.TabIndex = 1;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // sql_input
+            // 
+            this.sql_input.AcceptsReturn = true;
+            this.sql_input.Location = new System.Drawing.Point(10, 7);
+            this.sql_input.Margin = new System.Windows.Forms.Padding(2);
+            this.sql_input.Name = "sql_input";
+            this.sql_input.Size = new System.Drawing.Size(405, 20);
+            this.sql_input.TabIndex = 2;
+            this.sql_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Sql_input_KeyDown);
+            // 
+            // sql_Output
+            // 
+            this.sql_Output.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sql_Output.Location = new System.Drawing.Point(9, 56);
+            this.sql_Output.Margin = new System.Windows.Forms.Padding(2);
+            this.sql_Output.Name = "sql_Output";
+            this.sql_Output.RowTemplate.Height = 24;
+            this.sql_Output.Size = new System.Drawing.Size(499, 237);
+            this.sql_Output.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(526, 56);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(499, 237);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 318);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(386, 200);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // MergedView
+            // 
+            this.MergedView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MergedView.Location = new System.Drawing.Point(526, 318);
+            this.MergedView.Name = "MergedView";
+            this.MergedView.Size = new System.Drawing.Size(499, 200);
+            this.MergedView.TabIndex = 6;
+            // 
+            // carRental
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 530);
+            this.Controls.Add(this.MergedView);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.sql_Output);
+            this.Controls.Add(this.sql_input);
+            this.Controls.Add(this.submitBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "carRental";
+            this.Text = "Car Rental";
+            ((System.ComponentModel.ISupportInitialize)(this.sql_Output)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MergedView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -98,6 +123,8 @@
         private System.Windows.Forms.TextBox sql_input;
         private System.Windows.Forms.DataGridView sql_Output;
           private System.Windows.Forms.DataGridView dataGridView1;
-     }
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView MergedView;
+    }
 }
 
--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -3,156 +3,258 @@ using System.Data;
 using System.Windows.Forms;
 using System.Data.SQLite;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace WindowsFormsApp2
 {
      public partial class carRental : Form
      {
+        //insert into cars values ('11', 2019, 'ex', 95.15)
 
-          SqlConnection conn_London = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Hassan\\source\\repos\\WindowsFormsApp2\\WindowsFormsApp2\\bin\\Debug\\Database1.mdf;Integrated Security=True");
-          SQLiteConnection conn_Mehdi = new SQLiteConnection("Data Source=car_DB.db;Version=3;");
+        SqlConnection conn_London = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+        SQLiteConnection conn_Mehdi = new SQLiteConnection("Data Source=car_DB.db;Version=3;");
 
-          SqlCommand cmd;
-          SqlDataReader reader;
-          DataTable dataTable;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        DataTable dataTable;
 
-          public carRental()
-          {
-               InitializeComponent();
-               string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
-               string path = (System.IO.Path.GetDirectoryName(executable));
-               AppDomain.CurrentDomain.SetData("DataDirectory", path);
-          }
-
-
-          DataTable sqlQueryfetch_Mehdi(string sql)
-          {
-               string newsql = merge(sql);
-
-               DataSet ds = new DataSet();
-               var da = new SQLiteDataAdapter(newsql, conn_Mehdi);
-               da.Fill(ds);
-               sql_input.Text = String.Empty;
-
-               return ds.Tables[0];
-          }
-
-          DataTable sqlQueryfetch_London(string sql)
-          {
-               cmd = new SqlCommand();
-               dataTable = new DataTable();
-
-               cmd.CommandText = sql;
-               cmd.CommandType = CommandType.Text;
-               cmd.Connection = conn_London;
-
-               conn_London.Open();
-               reader = cmd.ExecuteReader();
-               dataTable.Load(reader);
-
-               conn_London.Close();
-               return dataTable;
-          }
-
-          string merge(string sql)
-          {
-               sql = sql.ToLower();
-               string newsql = "";
-
-               if (sql.Contains("cars"))
-               {
-                    newsql = sql.Replace("cars", "CAR_INFO");
-               }
-
-               if (sql.Contains("customers"))
-               {
-                    newsql = sql.Replace("customers", "CUSTOMER_INFO");
-               }
-
-               if (sql.Contains("rentals"))
-               {
-                    newsql = sql.Replace("rentals", "RENTAL_INFO");
-               }
-
-               return newsql;
-          }
-
-          DataTable convert2London(DataTable dt)
-          {
-               if (dt.Columns.Contains("year"))
-               {
-                    dt.Columns["car id"].ColumnName = "Vin";
-                    dt.Columns["year"].ColumnName = "Model_year";
-                    dt.Columns["rental price"].ColumnName = "Daily_Rate";
-
-               }
-
-               if (dt.Columns.Contains("Address"))
-               {
-                    dt.Columns["Drivers License"].ColumnName = "DriversLicenseNumber";
-                    dt.Columns["Address"].ColumnName = "Address";
-                    dt.Columns["Name"].ColumnName = "Full_Name";
-                    dt.Columns.Add("DateOfBirth", typeof(DateTime));
-               }
-
-               if (dt.Columns.Contains("Start Date"))
-               {
-                    dt.Columns["car id"].ColumnName = "Vin";
-                    dt.Columns["Drivers License"].ColumnName = "DriverseLicenseNumber";
-                    dt.Columns["Start Date"].ColumnName = "StartRentalDate";
-                    dt.Columns["End Date"].ColumnName = "EndRentalDate";
-               }
-
-               return dt;
-          }
-
-          private void submitBtn_Click(object sender, EventArgs e)
-          {
-               string query = sql_input.Text;
-               // try
-               //{
-               if (!String.IsNullOrEmpty(query))
-               {
-                    DataTable data_London = sqlQueryfetch_London(query);
-                    DataTable data_Mehdi = sqlQueryfetch_Mehdi(query);
-                    data_Mehdi = convert2London(data_Mehdi);
-
-                    //data_London.Merge(data_Mehdi);
-
-                    sql_Output.DataSource = data_London;
-                    dataGridView1.DataSource = data_Mehdi;
-
-               }
-               else
-               {
-                    MessageBox.Show("Please enter text before submission", "Empty Query",
-                                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
-               }
-               //     }
-               //     catch
-               //    {
-               //           MessageBox.Show("please review your query", "Invalid Query",
-               //          MessageBoxButtons.OK, MessageBoxIcon.Warning);
-               //   }
-          }
+        public carRental()
+        {
+            InitializeComponent();
+            string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string path = (System.IO.Path.GetDirectoryName(executable));
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
 
 
+        DataTable SqlQueryfetch_Mehdi(string sql)
+        {
+            string newsql = Merge(sql);
 
-          private void carRental_Load(object sender, EventArgs e)
-          {
-          }
+            DataSet ds = new DataSet();
+            var da = new SQLiteDataAdapter(newsql, conn_Mehdi);
+            da.Fill(ds);
+            sql_input.Text = String.Empty;
 
-          private void sql_input_KeyDown(object sender, KeyEventArgs e)
-          {
-               if (e.KeyCode == Keys.Enter)
-               {
-                    submitBtn_Click(sender, e);
-               }
-          }
+            return ds.Tables[0];
+        }
 
-          private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-          {
+        DataTable SqlQueryfetch_London(string sql)
+        {
+            cmd = new SqlCommand();
+            dataTable = new DataTable();
 
-          }
-     }
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn_London;
+
+            conn_London.Open();
+            reader = cmd.ExecuteReader();
+            dataTable.Load(reader);
+
+            conn_London.Close();
+            return dataTable;
+        }
+
+        string Merge(string sql)
+        {
+            sql = sql.ToLower();
+            string newsql = "";
+
+            if (sql.Contains("cars"))
+            {
+                newsql = sql.Replace("cars", "CAR_INFO");
+            }
+
+            if (sql.Contains("customers"))
+            {
+                newsql = sql.Replace("customers", "CUSTOMER_INFO");
+            }
+
+            if (sql.Contains("rentals"))
+            {
+                newsql = sql.Replace("rentals", "RENTAL_INFO");
+            }
+
+            return newsql;
+        }
+
+        DataTable MergeDB(DataTable Mehdi, DataTable London)
+        {
+            DataTable masterView = new DataTable();
+            if (Mehdi.Columns.Contains("Address"))
+            {
+                DataTable data_Mehdi_New = Mehdi.Clone();
+                data_Mehdi_New.Columns[0].DataType = typeof(Int64);
+                foreach (DataRow row in Mehdi.Rows)
+                {
+                    data_Mehdi_New.ImportRow(row);
+                }
+                data_Mehdi_New.AcceptChanges();
+                Mehdi = data_Mehdi_New;
+
+                Mehdi.Columns["Drivers License"].ColumnName = "DriversLicenseNumber";
+                Mehdi.Columns["Address"].ColumnName = "Address";
+                Mehdi.Columns["Name"].ColumnName = "Full_Name";
+                Mehdi.Columns.Add("DateOfBirth", typeof(string));
+
+                masterView = Mehdi.Clone();
+                foreach (DataRow dr in Mehdi.Rows)
+                {
+                    var DOB = dr.ItemArray[3].ToString();
+                    if (DOB == "")
+                    {
+                        dr.SetField<string>(3, "1/1/1970");
+                    }
+                    masterView.ImportRow(dr);
+
+                }
+                foreach (DataRow dr in London.Rows)
+                {
+                    var DOB = dr.ItemArray[3].ToString();
+                    var EndChar = DOB.IndexOf(' ');
+                    DOB = DOB.Substring(0, EndChar);
+                    dr.SetField<string>(3, DOB);
+                    masterView.ImportRow(dr);
+                }
+            }
+
+            else if (Mehdi.Columns.Contains("year"))
+            {
+                DataTable data_Mehdi_New = Mehdi.Clone();
+                data_Mehdi_New.Columns[0].DataType = typeof(string);
+                data_Mehdi_New.Columns[1].DataType = typeof(Int16);
+                data_Mehdi_New.Columns[2].DataType = typeof(string);
+                data_Mehdi_New.Columns[3].DataType = typeof(string);
+                foreach (DataRow row in Mehdi.Rows)
+                {
+                    data_Mehdi_New.ImportRow(row);
+                }
+                data_Mehdi_New.AcceptChanges();
+                Mehdi = data_Mehdi_New;
+
+                Mehdi.Columns["car id"].ColumnName = "Vin";
+                Mehdi.Columns["year"].ColumnName = "Model_year";
+                Mehdi.Columns["rental price"].ColumnName = "Daily_Rate";
+
+                masterView = Mehdi.Clone();
+                //foreach (DataColumn dc in Mehdi.Columns)
+                //{
+                //    richTextBox1.Text += $"{dc.DataType}\n";
+                //}
+                //foreach (DataColumn dc in London.Columns)
+                //{
+                //    richTextBox1.Text += $"{dc.DataType}\n";
+                //}
+                foreach (DataRow dr in Mehdi.Rows)
+                {
+                    masterView.ImportRow(dr);
+                }
+
+                foreach (DataRow dr in London.Rows)
+                {
+                    masterView.ImportRow(dr);
+                }
+            }
+
+            else if (Mehdi.Columns.Contains("Start Date"))
+            {
+                DataTable data_London_New = London.Clone();
+                data_London_New.Columns[1].DataType = typeof(string);
+                data_London_New.Columns[2].DataType = typeof(string);
+                data_London_New.Columns[3].DataType = typeof(string);
+                foreach (DataRow row in London.Rows)
+                {
+                    data_London_New.ImportRow(row);
+                }
+                data_London_New.AcceptChanges();
+                London = data_London_New;
+
+                Mehdi.Columns["car id"].ColumnName = "Vin";
+                Mehdi.Columns["Drivers License"].ColumnName = "DriverseLicenseNumber";
+                Mehdi.Columns["Start Date"].ColumnName = "StartRentalDate";
+                Mehdi.Columns["End Date"].ColumnName = "EndRentalDate";
+
+                masterView = Mehdi.Clone();
+
+                foreach (DataRow dr in Mehdi.Rows)
+                {
+                    masterView.ImportRow(dr);
+                }
+
+                foreach (DataRow dr in London.Rows)
+                {
+                    var startDate = dr.ItemArray[2].ToString();
+                    var s_EndChar = startDate.IndexOf(' ');
+                    startDate = startDate.Substring(0, s_EndChar);
+                    dr.SetField<string>(2, startDate);
+
+                    var endDate = dr.ItemArray[3].ToString();
+                    var e_EndChar = endDate.IndexOf(' ');
+                    endDate = endDate.Substring(0, e_EndChar);
+                    dr.SetField<string>(3, endDate);
+                    masterView.ImportRow(dr);
+                }
+
+            }
+
+            //foreach(DataColumn dc in Mehdi.Columns)
+            //{
+            //    richTextBox1.Text += $"{dc.DataType}\n";
+            //}
+            //foreach (DataColumn dc in London.Columns)
+            //{
+            //    richTextBox1.Text += $"{dc.DataType}\n";
+            //}
+
+
+            return masterView;
+        }
+
+
+        DataTable ConvertDateToString(DataTable dt)
+        {
+            DataTable data_London_new = dt.Clone();
+            data_London_new.Columns[3].DataType = typeof(string);
+            foreach (DataRow row in dt.Rows)
+            {
+                data_London_new.ImportRow(row);
+            }
+            data_London_new.AcceptChanges();
+            return data_London_new;
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            string query = sql_input.Text;
+
+            DataTable newTable;
+            if (!String.IsNullOrEmpty(query))
+            {
+                DataTable data_London = SqlQueryfetch_London(query);
+                DataTable data_Mehdi = SqlQueryfetch_Mehdi(query);
+                data_London = ConvertDateToString(data_London);
+                newTable = MergeDB(data_Mehdi, data_London);
+
+
+                sql_Output.DataSource = data_London;
+                dataGridView1.DataSource = data_Mehdi;
+                MergedView.DataSource = newTable;
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter text before submission", "Empty Query",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Sql_input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SubmitBtn_Click(sender, e);
+            }
+        }
+    }
 }
